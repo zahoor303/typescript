2f59cbf580da916257155400f0171dbe94db44e4
ZAHOOR FATIMA <160278589+zahoor303@users.noreply.github.com>
2024-02-27 14:41:06 +0500

    Excerise 9

--- app.js
+++ app.js
@@ -1,4 +1,2 @@
-console.log(5 + 3);
-console.log(11 - 3);
-console.log(4 * 2);
-console.log(16 / 2);
+var favouriteNumber = 3;
+console.log("My favourite number is ".concat(favouriteNumber));
--- app.ts
+++ app.ts
@@ -1,7 +1,2 @@
-console.log(5+3)
-console.log(11-3)
-console.log(4*2)
-console.log(16/2)
-
-
-
+let favouriteNumber: number = 3;
+console.log(`My favourite number is ${favouriteNumber}`);
