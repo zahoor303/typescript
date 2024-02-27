e1a6d78ef78b113c0688f10df8cdea9cb61a6d5f
ZAHOOR FATIMA <160278589+zahoor303@users.noreply.github.com>
2024-02-27 14:46:29 +0500

    Excerise 13

--- app.js
+++ app.js
@@ -1,11 +1,4 @@
-// Ex 11
-// let bestfriends:string[] = [`Qira`, `fakhra`, `husna`, `javeria`];
-// for(let i=0; i<bestfriends.length; i++){
-//   console.log(bestfriends[i]);
-// }
-// Ex 12
-var bestfriends = ["Qira", "fakhra", "husna", "javeria"];
-var message = "You are invited for a farewell party on wednesday ";
-for (var i = 0; i < bestfriends.length; i++) {
-    console.log(message + bestfriends[i]);
+var transportation = ["car", "bike", "bus", "helicoptor"];
+for (var i = 0; i < transportation.length; i++) {
+    console.log("I would like to own a " + transportation[i]);
 }
--- app.ts
+++ app.ts
@@ -1,14 +1,4 @@
-// Ex 11
-// let bestfriends:string[] = [`Qira`, `fakhra`, `husna`, `javeria`];
-// for(let i=0; i<bestfriends.length; i++){
-//   console.log(bestfriends[i]);
-// }
-
-
-// Ex 12
-
-let bestfriends:string[] = [`Qira`, `fakhra`, `husna`, `javeria`];
-let message:string = `You are invited for a farewell party on wednesday `;
-for(let i=0; i<bestfriends.length; i++){
-  console.log(message+bestfriends[i]);
- }
+let transportation: string[] = [`car`,`bike` ,`bus`,`helicoptor`];
+for(let i=0; i<transportation.length; i++){
+  console.log(`I would like to own a ` +  transportation[i]);
+}
