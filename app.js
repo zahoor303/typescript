19e331928fda1da577b502382811a65d16f42c7a
ZAHOOR FATIMA <160278589+zahoor303@users.noreply.github.com>
2024-02-27 14:37:52 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var personName = "\n\t ZAHOOR FATIMA \t\n";
-console.log(personName);
-var stripped = personName.trim();
-console.log(stripped);
+console.log(5 + 3);
+console.log(11 - 3);
+console.log(4 * 2);
+console.log(16 / 2);
--- app.ts
+++ app.ts
@@ -1,4 +1,7 @@
-let personName: string = `\n\t ZAHOOR FATIMA \t\n`;
-console.log(personName);
-let stripped: string = personName.trim();
-console.log(stripped);
+console.log(5+3)
+console.log(11-3)
+console.log(4*2)
+console.log(16/2)
+
+
+
